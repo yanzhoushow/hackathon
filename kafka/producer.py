e40d52d2dcf5b3721af2f28aed4058921b4c6206
Yan Zhou <38987117+yanzhoushow@users.noreply.github.com>
2021-04-07 22:11:14 -0400

    Create producer.py

---
+++ kafka/producer.py
@@ -0,0 +1,60 @@
+    topic = args.topic
+
+    schema_str = """
+    {
+      "$schema": "http://json-schema.org/draft-07/schema#",
+      "title": "User",
+      "description": "A Confluent Kafka Python User",
+      "type": "object",
+      "properties": {
+        "name": {
+          "description": "User's name",
+          "type": "string"
+        },
+        "favorite_number": {
+          "description": "User's favorite number",
+          "type": "number",
+          "exclusiveMinimum": 0
+        },
+        "favorite_color": {
+          "description": "User's favorite color",
+          "type": "string"
+        }
+      },
+      "required": [ "name", "favorite_number", "favorite_color" ]
+    }
+    """
+    schema_registry_conf = {'url': schema_registry_url}
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+
+    json_serializer = JSONSerializer(schema_str, schema_registry_client, user_to_dict)
+
+    producer_conf = {'bootstrap.servers': args.bootstrap_servers,
+                     'key.serializer': StringSerializer('utf_8'),
+                     'value.serializer': json_serializer}
+
+    producer = SerializingProducer(producer_conf)
+
+    print("Producing user records to topic {}. ^C to exit.".format(topic))
+    while True:
+        # Serve on_delivery callbacks from previous calls to produce()
+        producer.poll(0.0)
+        try:
+            user_name = input("Enter name: ")
+            user_address = input("Enter address: ")
+            user_favorite_number = int(input("Enter favorite number: "))
+            user_favorite_color = input("Enter favorite color: ")
+            user = User(name=user_name,
+                        address=user_address,
+                        favorite_color=user_favorite_color,
+                        favorite_number=user_favorite_number)
+            producer.produce(topic=topic, key=str(uuid4()), value=user,
+                             on_delivery=delivery_report)
+        except KeyboardInterrupt:
+            break
+        except ValueError:
+            print("Invalid input, discarding record...")
+            continue
+
+    print("\nFlushing records...")
+    producer.flush()
